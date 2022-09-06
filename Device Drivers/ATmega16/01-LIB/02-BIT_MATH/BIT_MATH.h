a7bf0aca80a0b0aa710032cd3a0b32ada3582194
Mohamed Abd El-Naby <mahameda.naby@gmail.com>
2022-09-06 21:49:42 +0200

    Updata BIT_MATH.h File

--- Device Drivers/ATmega16/01-LIB/02-BIT_MATH/BIT_MATH.h
+++ Device Drivers/ATmega16/01-LIB/02-BIT_MATH/BIT_MATH.h
@@ -1,7 +1,7 @@
 /**************************************************************************/
 /* Author	: Mohamed                                               	  */
 /* Date		: 26 January 2021                                 		      */
-/* Version	: V01							  							  */
+/* Version	: V02							  							  */
 /**************************************************************************/
 
 #ifndef BIT_MATH_H
@@ -10,7 +10,10 @@
 	#define SET_BIT(REG,BIT_NUM)	REG |= (1 << BIT_NUM)
 	#define CLR_BIT(REG,BIT_NUM)	REG &= ~(1<<BIT_NUM)
 	#define TOG_BIT(REG,BIT_NUM)	REG ^= (1 << BIT_NUM)
-	#define GET_BIT(REG,BIT_NUM)	REG  =((1 >>  BIT_NUM) & 1	)		
+	#define GET_BIT(REG,BIT_NUM)	((REG >>  BIT_NUM) & 1	)
+	//#define GET_BIT(REG,BIT_NUM)	((REG & (1<<BIT_NUM) )>>BIT_NUM) 
+
+			
 
 
 
